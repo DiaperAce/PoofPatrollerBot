cc2bd5e320e20a31ac589b8be386cb88ee692978
Ace <35043186+DiaperAce@users.noreply.github.com>
2018-01-02 14:03:09 -0800

    Update poof_patroller_bot.py
    Included support for more than one type of response per status

--- poof_patroller_bot.py
+++ poof_patroller_bot.py
@@ -6,10 +6,10 @@
 
 Poof Patroller Bot
 
-Basically Rolls a dice and 
+Basically Rolls a dice and returns a status
 """
 
-from uuid import uuid4
+#from uuid import uuid4
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram import InlineQueryResultArticle, InputTextMessageContent
@@ -21,34 +21,40 @@ import logging
 ############
 #Parameters#
 ############
-authToken = 'TOKEN_GOES_HERE' #a token that lets Python communicate with the bot
+authToken = '545347539:AAFKq0GLSZgLNUB8p1zM4URrhM92TJfyWgo' #a token that lets Python communicate with the bot
 
 ##############
 #Py Functions#
 ##############
 
 def roll():
+    """
+    prng
+    """
     nums=[[i] for i in range(6)]
     rand.shuffle(nums)
-    return nums[0][0]
+    return int(nums[0][0])
 
-def check_pamps():
+def check_pamps(username):
+    """
+    dictionary that lets us look up statuses and pick one.
+    """
     #Roll the dice
     diapercheck = roll()
     
     #Apply the result
-    if diapercheck == 0:
-        status = " is wet and messy. Bummer, kiddo. Looks like you won't be out of those diapers any time soon."
-    elif diapercheck == 1:
-        status = " is messy. Oof! That smell is overpowering."
-    elif diapercheck == 2:
-        status = " is soaked! Chance of leaking at 95% if not changed immediately."
-    elif diapercheck == 3:
-        status = " is wet, but their diaper can still hold quite a bit more."
-    elif diapercheck == 4:
-        status = " is a little damp. Looks like they're not as big as they think"
-    elif diapercheck == 5:
-        status = " is clean. What a big kid!"
+    str(username)
+    status_dic ={
+        0: ["@"+username+" is wet and messy. Bummer, kiddo. Looks like you won't be out of those diapers any time soon."],
+        1: ["@"+username+" is messy. Oof! That smell is overpowering."],
+        2: ["@"+username+" is soaked! Chance of leaking at 95% if not changed immediately.",
+            "Oh no! @"+username+" is about to leak! Someone needs chanigng pronto!"],
+        3: ["@"+username+" is wet, but their diaper can still hold quite a bit more."],
+        4: ["@"+username+" is a little damp. Looks like they're not as big as they think."],
+        5: ["@"+username+" is clean. What a big kid!"]
+        }
+    
+    status = rand.choice(status_dic[diapercheck])
     return status
 
 #############
@@ -69,13 +75,14 @@ def start(bot, update):
 
 def check(bot, update,args):
     """
-    /check function. Genetrates a random number between 1 and 6, and reports the status of someone's diaper based on the results
+    /check function. Genetrates a random number between 0 and 5, and reports the status of someone's diaper based on the results
     """
     #Read username
     if not args:
         M1 = "Thank you for choosing Poof Patroller Bot for all your diaper checking needs. Please specify a person's diaper to check using the syntax \n\n/check @username."
         bot.send_message(chat_id=update.message.chat_id, text=M1)
         return
+    #takes care of the @
     if args[0][0]=="@":
         bab=str(args[0][1::])
     else:
@@ -95,14 +102,12 @@ def check(bot, update,args):
     bot.send_message(chat_id=update.message.chat_id, text=M3)
     sleep(2)#etc
     
-    status = check_pamps()
-    
-    outText = "@" + bab + status
+    outText = check_pamps(bab)
     #send output to bot
     bot.send_message(chat_id=update.message.chat_id, text=outText)
     return
 
-
+"""
 def inline_check(bot, update):
     '''Handle the inline query.'''
     bab = update.inline_query.query
@@ -144,7 +149,7 @@ def inline_check(bot, update):
     update.inline_query.answer(results)
     
     return
-        
+"""        
 
 ######
 #Main#
